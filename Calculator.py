8ee9fc009cdfaf45fb3715619fc4be8eea7f485a
AnkitaDayalSingh <anjudy145@gmail.com>
2026-06-11 22:29:44 +0530

    error handling

--- Calculator.py
+++ Calculator.py
@@ -9,30 +9,36 @@ def operation(x, y, op):
         result = x * y
     elif op in ["/", "÷"]:
         if y == 0:
-            result = "Can not divide by zero"
+            result = "~ Cannot divide by zero ~"
         else:
             result = x / y
     elif op in ["^","**"]:
         result = x**y
     else:
-        result = "Invalid Operator"
+        result = "~ Invalid Operator ~"
     return result
 
 while True:
     print("1 New Calculation")
     print("0 Exit")
-    a = int(input("Enter: "))
 
-    if a == 1:
-        x = int(input("First Number: "))
-        op = input("Select Operator: ")
-        y = int(input("Second Number: "))
-        
-        output = operation(x, y, op)
-        print(output)
+    try:
+        a = int(input("Enter: "))
+
+        if a == 1:
+            x = int(input("First Number: "))
+            op = input("Select Operator: ")
+            y = int(input("Second Number: "))
+            
+            output = operation(x, y, op)
+            print(output)
     
-    elif a == 0:
-        break
-    else:
-        print("Invalid Input")
+        elif a == 0:
+            break
+        else:
+            print("~ Invalid Input ~")
+            continue
+
+    except ValueError:
+        print("~ Invalid Input ~")
         continue
