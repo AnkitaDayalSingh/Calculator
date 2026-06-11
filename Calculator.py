00be1e5481fac5bb312fe9357198c31cdcfc5abf
AnkitaDayalSingh <anjudy145@gmail.com>
2026-06-11 22:08:34 +0530

    menu

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,36 @@
 #Calculator
 
-x = int(input("First Number: "))
-op = input("Select Operator: ")
-y = int(input("Second Number: "))
-
-if op == '+':
-    result = x + y
-elif op == "-":
-    result = x - y
-elif op in ["*", "×"]:
-    result = x * y
-elif op in ["/", "÷"]:
-    if y == 0:
-        result = "Can not divide by zero"
+def operation(x, y, op):
+    if op == '+':
+        result = x + y
+    elif op == "-":
+        result = x - y
+    elif op in ["*", "×"]:
+        result = x * y
+    elif op in ["/", "÷"]:
+        if y == 0:
+            result = "Can not divide by zero"
+        else:
+            result = x / y
     else:
-        result = x / y
+        result = "invalid operator"
+    return result
 
-print(result)
+while True:
+    print("1 New Calculation")
+    print("0 Exit")
+    a = int(input("Enter: "))
+
+    if a == 1:
+        x = int(input("First Number: "))
+        op = input("Select Operator: ")
+        y = int(input("Second Number: "))
+        
+        output = operation(x, y, op)
+        print(output)
+    
+    elif a == 0:
+        break
+    else:
+        print("Invalid Input")
+        continue
