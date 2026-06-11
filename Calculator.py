e38db8612825d557660f6ca55a429896d1529a5e
AnkitaDayalSingh <anjudy145@gmail.com>
2026-06-11 18:58:06 +0530

    intial file

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+#Calculator
+
+x = int(input("First Number: "))
+op = input("Select Operator: ")
+y = int(input("Second Number: "))
+
+if op == '+':
+    result = x + y
+elif op == "-":
+    result = x - y
+elif op == "*":
+    result = x * y
+elif op == "/":
+    if y == 0:
+        result = "Can not divide by zero"
+    else:
+        result = x / y
+
+print(result)
